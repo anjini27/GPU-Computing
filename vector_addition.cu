074f9bc901f6c9680658362abf5503a93fc4b5f2
Anjini Bhukya <158824001+anjini27@users.noreply.github.com>
2026-01-23 23:10:04 +0530

    Improve vector addition with CUDA memory handling
    Refactored vector addition kernel and added memory management.

--- vector_addition.cu
+++ vector_addition.cu
@@ -1,28 +1,34 @@
-#include <stdio.h>
 #include <cuda_runtime.h>
 
 __global__ void vector_add(const float* A, const float* B, float* C, int N) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (idx < N) {
-        C[idx] = A[idx] + B[idx];
-
-        
-         printf("C[%d] = %f\n", idx, C[idx]);
-    }
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < N)
+        C[i] = A[i] + B[i];
 }
 
 extern "C" void solve(const float* A, const float* B, float* C, int N) {
+
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    int size = N * sizeof(float);
 
-   
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        printf("CUDA kernel error: %s\n", cudaGetErrorString(err));
-    }
+    float *d_A, *d_B, *d_C;
 
+    cudaMalloc((void**)&d_A, size);
+    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_B, size);
+    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_C, size);
+
+    vector_add<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
     cudaDeviceSynchronize();
+
+    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
